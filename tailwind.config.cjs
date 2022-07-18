2b479429d5c2d7cfbb0818ba0ce8b7b851bfd9e9
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2022-07-18 21:14:40 +0200

    :lipstick: extend colors in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				red: {
+					pen: "#FF0000"
+				},
+				blue: {
+					pen: "#0000FF"
+				},
+				green: {
+					pen: "#00FF00"
+				},
+				yellow: {
+					pen: "#FFFF00"
+				},
+				black: "#000000",
+			}
+		}
 	},
 
 	plugins: []
