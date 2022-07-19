de8a370eab8030b05b1c6e336663e87306e71255
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2022-07-19 21:46:39 +0200

    :construction: draw line feature

--- src/lib/scripts/draw.service.ts
+++ src/lib/scripts/draw.service.ts
@@ -1,10 +1,56 @@
-export const draw: DrawEvent = (ctx, options, event) => {
-	ctx.lineWidth = options.size;
-	ctx.lineCap = options.lineCap;
-	ctx.strokeStyle = options.color;
+const handleOptionsHelper: (arg0: CanvasRenderingContext2D, arg1: Options) => void = (
+	ctx,
+	options
+) => {
+	if (options.color) {
+		ctx.fillStyle = options.color;
+	}
+	if (options.lineCap) {
+		ctx.lineCap = options.lineCap;
+	}
+	if (options.size) {
+		ctx.lineWidth = options.size;
+	}
+};
 
-	ctx.lineTo(event.clientX, event.clientY);
+const drawModePen: DrawFn = (ctx, x, y) => {
+	ctx.lineTo(x, y);
 	ctx.stroke();
 	ctx.beginPath();
-	ctx.moveTo(event.clientX, event.clientY);
+	ctx.moveTo(x, y);
+};
+
+const drawModeLine: DrawFn = (ctx, x, y, params) => {
+	if (params === null || params === undefined) {
+		throw new Error('Params must be defined');
+	}
+	const {
+		event: { clientX: startX },
+		event: { clientY: startY }
+	} = params;
+
+	if (params.currentImage === null) {
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	} else {
+		ctx.putImageData(params.currentImage, 0, 0);
+	}
+	ctx.moveTo(startX, startY);
+	ctx.beginPath();
+	ctx.lineTo(x, y);
+	ctx.stroke();
+};
+
+export const draw: DrawEvent = (ctx, options, params, event) => {
+	const { clientX: x, clientY: y } = event;
+
+	handleOptionsHelper(ctx, options);
+
+	switch (options.drawMode) {
+		case 'pen':
+			drawModePen(ctx, x, y);
+			break;
+		case 'line':
+			drawModeLine(ctx, x, y, params);
+			break;
+	}
 };
