294faeeeb8b8db3df1cf7e39f2bf3d7033373025
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2022-07-19 21:52:45 +0200

    :sparkles: add simple debug text

---
+++ src/lib/components/Debug.local.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { history } from '$lib/stores/whiteboardHistory.store';
+	import { options } from '$lib/stores/whiteboardOptions.store';
+	import { params } from '$lib/stores/whiteboardParams.store';
+</script>
+
+<div class="w-48 absolute top-0 right-0 p-4">
+  {#if $options !== null || $options !== undefined}
+    <div class="font-medium">Options:</div>
+    <ul>
+      <li>color: {$options.color}</li>
+      <li>size: {$options.size}</li>
+      <li>lineCap: {$options.lineCap}</li>
+      <li>drawMode: {$options.drawMode}</li>
+    </ul>
+  {/if}
+  {#if $params !== null && $params !== undefined}
+  <div class="font-medium">Anchor coordinates:</div>
+    <ul>
+      <li>x: {$params.startPoint.x}</li>
+      <li>y: {$params.startPoint.y}</li>
+    </ul>
+  {/if}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,11 @@
 import { onMount } from "svelte";
 import Canvas from "$lib/components/Canvas.svelte";
 import ContextMenu from "$lib/components/ContextMenu.svelte";
+import Debug from "$lib/components/Debug.local.svelte";
 
 </script>
 <div>
   <ContextMenu />
+  <Debug /> 
   <Canvas />
 </div>
